f772676466d407dbe67745a750278c9056eb0207
juanpablo <juanpablo2007k@gmail.com>
2023-08-27 13:43:06 -0500

    firts proyecto form

--- form.php
+++ form.php
@@ -1,29 +1,57 @@
 <?php
-
-
 require_once("$CFG->libdir/formslib.php");
 
 class simplehtml_form extends moodleform {
-    //Add elements to form
+    // Add elements to form
     public function definition() {
         global $CFG;
-       
-        $mform = $this->_form; // Don't forget the underscore! 
-        $mform->addElement('text', 'email', get_string('email')); // Add elements to your form.
-        $mform->setType('email', PARAM_NOTAGS);                   // Set type of element.
-        $mform->setDefault('email', '');        // Default value.
 
-        $mform = $this->_form; // Don't forget the underscore! 
-        $mform->addElement('text', 'phone', get_string('phone')); // Add elements to your form.
-        $mform->setType('phone', PARAM_NOTAGS);                   // Set type of element.
-        $mform->setDefault('phone', '');        // Default value.
+        $mform = $this->_form; // No olvides el guión bajo.
+
+        $mform->addElement('text', 'username', get_string('username'));
+        $mform->setType('username', PARAM_NOTAGS);
+        $mform->setDefault('username', '');
+
+        $mform->addElement('text', 'email', get_string('email'));
+        $mform->setType('email', PARAM_NOTAGS);
+        $mform->setDefault('email', '');
+
+        $mform->addElement('text', 'phone', get_string('phone'));
+        $mform->setType('phone', PARAM_NOTAGS);
+        $mform->setDefault('phone', '');
+
+        $mform->addElement('text', 'firstname', get_string('firstname'));
+        $mform->setType('firstname', PARAM_NOTAGS);
+        $mform->setDefault('firstname', '');
+
+        $mform->addElement('text', 'lastname', get_string('lastname'));
+        $mform->setType('lastname', PARAM_NOTAGS);
+        $mform->setDefault('lastname', '');
+
+        $mform->addElement('text', 'city', get_string('city'));
+        $mform->setType('city', PARAM_NOTAGS);
+        $mform->setDefault('city', '');
+
+        $mform->addElement('text', 'country', get_string('country'));
+        $mform->setType('country', PARAM_NOTAGS);
+        $mform->setDefault('country', '');
+
+        $mform->addElement('textarea', 'descripcion', get_string('descripcion'));
+        $mform->setType('descripcion', PARAM_TEXT);
+        $mform->setDefault('descripcion', '');
+
+$mform->addElement('text', 'user_id', 'ID del Usuario');
+$mform->setType('user_id', PARAM_INT);
+$mform->addElement('text', 'course_id', 'ID del Curso');
+$mform->setType('course_id', PARAM_INT);
+
+
         $this->add_action_buttons();
     }
-    
-    //Custom validation should be added here
+
+    // Custom validation should be added here
     function validation($data, $files) {
         return array();
     }
 }
-
 ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('../config.php');
 require_login();
 
-global $CFG, $DB, $USER, $OUTPUT, $PAGE; 
+global $CFG, $DB, $USER, $OUTPUT, $PAGE;
 
 require_once($CFG->dirroot . "/test/form.php");
 $PAGE->set_url(new moodle_url('/test/index.php'));
@@ -15,22 +15,56 @@ $mform = new simplehtml_form();
 
 if ($mform->is_cancelled()) {
     $cancelado = $CFG->wwwroot . "/test/index.php";
-    redirect($cancelado, 'Has cancelado la envío de datos', null, \core\output\notification::NOTIFY_WARNING);
-} else if ($fromform = $mform->get_data()) {
-    print_r($fromform);
-    $date = new stdClass();
-    $date->email = $fromform->email;
-    $date->phone = $fromform->phone;
-    $date->added_time = time();
-    $date->added_by = $USER->id; // Esto asume que $USER->id contiene el ID del usuario actual, asegúrate de que esté configurado correctamente    
-    $DB->insert_record('example', $date);
-
-    // Redirigir antes de enviar cualquier contenido al navegador
-    $redirigir = $CFG->wwwroot . "/test/index.php";
-    redirect($redirigir, 'Se ha enviado la información correctamente', null, \core\output\notification::NOTIFY_SUCCESS);
+    redirect($cancelado, 'Has cancelado el envío de datos', null, \core\output\notification::NOTIFY_WARNING);
 } else {
     echo $OUTPUT->header();
     $mform->display();
+
+    if ($fromform = $mform->get_data()) {
+        // Crear un nuevo usuario en la base de datos de Moodle.
+        $user = new stdClass();
+        $user->auth = 'manual';
+        $user->confirmed = 1;
+
+        // Campos de usuario
+        $user->username = $fromform->username; // Nombre de usuario
+        $user->password = ''; // Debes manejar el cifrado y almacenamiento de la contraseña aquí.
+        $user->firstname = $fromform->firstname; // Nombre
+        $user->lastname = $fromform->lastname; // Apellido
+        $user->email = $fromform->email; // Correo electrónico
+
+        // Otras configuraciones de usuario aquí...
+
+        // Insertar el usuario en la tabla de usuarios.
+        $user_id = $DB->insert_record('user', $user);
+
+        if ($user_id) {
+            // Obtener el ID del curso desde el formulario.
+            $course_id = $fromform->course_id;
+
+            // Matricular al usuario en el curso.
+            $enrol = $DB->get_record('enrol', ['courseid' => $course_id, 'enrol' => 'manual'], '*', MUST_EXIST);
+            $enrolment = new stdClass();
+            $enrolment->enrolid = $enrol->id;
+            $enrolment->userid = $user_id;
+            $enrolment->timecreated = time();
+            $enrolment->timemodified = $enrolment->timecreated;
+            $enrolment->modifierid = $USER->id;
+
+            if ($DB->insert_record('user_enrolments', $enrolment)) {
+                echo "El usuario ha sido matriculado en el curso con éxito.";
+            } else {
+                echo "Error al matricular al usuario en el curso.";
+            }
+        } else {
+            echo "Error al insertar el usuario en la base de datos.";
+        }
+
+        // Redirigir antes de enviar cualquier contenido al navegador.
+        $redirigir = $CFG->wwwroot . "/test/index.php";
+        redirect($redirigir, 'Se ha enviado la información correctamente', null, \core\output\notification::NOTIFY_SUCCESS);
+    }
+
     echo $OUTPUT->footer();
 }
 ?>
